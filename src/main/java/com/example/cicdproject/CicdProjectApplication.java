44446a7ed16325f2bb2a50a31b777149269546ee
ziogenorwekh <ziogenorwekh@gmail.com>
2023-04-08 19:06:05 +0900

    Initial commit

---
+++ src/main/java/com/example/cicdproject/CicdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicdproject/CicdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
