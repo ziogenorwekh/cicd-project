dfeaefccd4e98254f6d46ef24879c4b1360cf993
ziogenorwekh <ziogenorwekh@gmail.com>
2023-04-10 21:46:20 +0900

    test

--- src/main/java/com/example/cicdproject/CicdProjectApplication.java
+++ src/main/java/com/example/cicdproject/CicdProjectApplication.java
@@ -2,12 +2,17 @@ package com.example.cicdproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdProjectApplication {
+public class CicdProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CicdProjectApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CicdProjectApplication.class);
+    }
 }
